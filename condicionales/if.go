b8f05b3b52ab7f85510760679aa0e5158d2d53f4
ElmerAlay <alayelmer1993@gmail.com>
2020-06-15 14:08:37 -0600

    conditions

---
+++ condicionales/if.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	if 5 == 10 {
+		fmt.Println("true")
+	} else if 5 == 5 {
+		fmt.Println("else if")
+	} else {
+		fmt.Println("False")
+	}
+
+	// --------------------------------------------------------------------
+	// ------------------------------- Notas ------------------------------
+	// --------------------------------------------------------------------
+	// paréntesis opcionales
+	// puede ir == != < > <= >= && || !
+	// la llave debe ir en la misma linea del if
+}
--- hellow_world/hello.go
+++ hellow_world/hello.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
+	// ; opcional
 	fmt.Printf("Hello, World!\n")
 }
